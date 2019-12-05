38e2e8a4bd11dd38d8a2d69ece49a6caf9f52035
MattK159 <matthew.keating2@mycit.ie>
2019-12-05 16:24:51 +0000

    Fixed medai pages

--- app.py
+++ app.py
@@ -98,9 +98,7 @@ def diabetes():
 
 @app.route('/user_profiling')
 def user_profiling():
-    records = {"times_pregnant": "2", "glucose": "16.5", "blood_pressure": "110", "skin_fold": "1.25", "bmi": "30.2", "ped_function": "4", "age": "25"}, {"times_pregnant": "4", "glucose": "45", "blood_pressure": "120", "skin_fold": "2.2", "bmi": "25.2", "ped_function": "2", "age": "55"}
-
-    return render_template('pages/UserProfiling.html', records=records)
+    return render_template('pages/UserProfiling.html')
 
 @app.route('/medi_Ai_Interface')
 def medi_ai_interface():
